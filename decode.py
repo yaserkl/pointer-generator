c4bf5848beac40b11d68b2bafc3059988e425cc1
Yaser Keneshloo <yaserkeneshloo@Yasers-MacBook-Air.local>
2017-12-03 15:46:26 -0500

    rouge eval added

--- decode.py
+++ decode.py
@@ -57,8 +57,8 @@ class BeamSearchDecoder(object):
       # Make a descriptive decode directory name
       ckpt_name = "ckpt-" + ckpt_path.split('-')[-1] # this is something of the form "ckpt-123456"
       self._decode_dir = os.path.join(FLAGS.log_root, get_decode_dir_name(ckpt_name))
-      if os.path.exists(self._decode_dir):
-        raise Exception("single_pass decode directory %s should not already exist" % self._decode_dir)
+      #if os.path.exists(self._decode_dir):
+      #  raise Exception("single_pass decode directory %s should not already exist" % self._decode_dir)
 
     else: # Generic decode dir name
       self._decode_dir = os.path.join(FLAGS.log_root, "decode")
